8a6f8b13d0d1a99f0a5c4a63feb59fb9161491c1
Gennady Dogaev <gendelflg@gmail.com>
2019-12-14 21:41:10 +0200

    Russian translations for timer

--- src/components/HomePage/Timer/Timer.ru.js
+++ src/components/HomePage/Timer/Timer.ru.js
@@ -1,3 +1,13 @@
-const ru = {};
+const ru = {
+  taskDescriptionPlaceholder:
+    "Что вы собираетесь делать? (Введите краткое описание задачи)",
+  projectPlaceholder: "Проект",
+  tagsPlaceholder: "Теги, через запятую",
+  hourlyRatePlaceholder: "Почасовой рейт",
+  startButton: "Старт",
+  stopButton: "Стоп",
+  syncError: "Ошибка синхронизации с Google документом: {{error}}",
+  syncRetryButton: "Попробовать ещё раз"
+};
 
 export default ru;

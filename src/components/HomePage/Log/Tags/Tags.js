0d6360c4111b6430c1e0d7fad494d8f97257360c
Gennady Dogaev <gendelflg@gmail.com>
2020-01-04 00:30:04 +0200

    Updated uikit and tags

--- src/components/HomePage/Log/Tags/Tags.js
+++ src/components/HomePage/Log/Tags/Tags.js
@@ -16,22 +16,13 @@ const Tags = ({ tags, definedTags, fetchState }) => {
       {tags.map((tag, index) => {
         const definedTag = definedTags.filter(({ name }) => name === tag)[0];
 
-        if (definedTag && definedTag.colorRGB) {
-          return (
-            <span
-              key={definedTag.uuid || index}
-              className="uk-label uk-margin-small-right uk-margin-small-bottom"
-              style={{ background: definedTag.colorRGB }}
-            >
-              {tag}
-            </span>
-          );
-        }
-
         return (
           <span
-            key={index}
+            key={(definedTag && definedTag.uuid) || index}
             className="uk-label uk-margin-small-right uk-margin-small-bottom"
+            style={{
+              background: (definedTag && definedTag.colorRGB) || null
+            }}
           >
             {tag}
           </span>

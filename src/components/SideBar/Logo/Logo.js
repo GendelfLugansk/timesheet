637a05ea7cd27abbfd8a0aadfb58032b5ea77bae
Gennady Dogaev <gendelflg@gmail.com>
2020-01-19 18:27:46 +0200

    Align logo

--- src/components/SideBar/Logo/Logo.js
+++ src/components/SideBar/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Logo = () => (
-  <div className="uk-text-uppercase uk-text-bold uk-text-emphasis uk-text-lead">
+  <div className="uk-text-uppercase uk-text-bold uk-text-emphasis uk-text-lead uk-text-middle">
     <img
-      className="uk-background-default uk-margin-small-right"
-      data-src={process.env.PUBLIC_URL + "/logo512.png"}
-      width="25"
-      height="25"
+      data-src={process.env.PUBLIC_URL + "/logo512-white.png"}
+      width="30"
+      height="30"
       alt=""
-      uk-img={process.env.PUBLIC_URL + "/logo512.png"}
+      uk-img={process.env.PUBLIC_URL + "/logo512-white.png"}
+      style={{ position: "relative", top: "-2px" }}
     />
     Timesheet
     <hr />
